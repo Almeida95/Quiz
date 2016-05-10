5c3c11e9e5f82a88435f7805cfd25134e273ca66
Almeida95 <martinezalmeida95@gmail.com>
2016-05-10 19:01:44 +0200

    Tema 20:Gestion de usuarios

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
-var commentController= require('../controllers/comment_Controller')
+var commentController= require('../controllers/comment_controller');
+var userController= require('../controllers/user_controller')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,6 +16,7 @@ router.get('/author', function(req, res, next) {
 
 //Autoload de rutas que usen :quizId
 router.param('quizId', quizController.load);  //autoload:quizId
+router.param('userId', userController.load); //autoload:userId
 
 //Definicion de rutas de /quizzes
 
@@ -27,6 +29,18 @@ router.get('/quizzes/:quizId(\\d+)/edit',   quizController.edit);
 router.put('/quizzes/:quizId(\\d+)',        quizController.update);
 router.delete('/quizzes/:quizId(\\d+)',        quizController.destroy);
 //router.get('/quizzes(\\w)',              quizController.search);  //****
+
+//Definicion de rutas de cuenta
+
+router.get('/quizzes',                     quizController.index);
+router.get('/quizzes/:quizId(\\d+)',       quizController.show);
+router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
+router.get('/quizzes/new',                 quizController.new);
+router.post('/quizzes',                    quizController.create);
+router.get('/quizzes/:quizId(\\d+)/edit',  quizController.edit);
+router.put('/quizzes/:quizId(\\d+)',       quizController.update);
+router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
+
  
 router.get('/quizzes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizzes/:quizId(\\d+)/comments',    commentController.create);
